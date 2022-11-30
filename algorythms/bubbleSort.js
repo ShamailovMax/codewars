62caca0e06b23642d6183878423868e6f7eb7d35
ShamailovMax <max24june@gmail.com>
2022-11-30 03:12:16 +0200

    new java task

---
+++ algorythms/bubbleSort.js
@@ -0,0 +1,14 @@
+// const bubbleSort = (arr) => {
+//   for (let i = arr.length - 1; i >= 1; i--) {
+//     for (let j = 0; j < i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let buffer = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = buffer;
+//       }
+//     }
+//   }
+//   return arr
+// };
+
+// console.log(bubbleSort([1, 0, 3, 10, -14, 7]))
